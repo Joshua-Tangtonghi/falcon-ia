02af65aa8d7b23d06793483a4f4c41abe5e1a9eb
1Doms <domsun78@gmail.com>
2025-11-05 14:44:23 +0100

    FalconIADom

---
+++ Assets/Teams/FalconIa/FalconIADom/FalconIADom.cs
@@ -0,0 +1,21 @@
+using DoNotModify;
+using UnityEngine;
+
+namespace Teams.ExampleTeam
+{
+    public class FalconIADom : BaseSpaceShipController
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ Assets/Teams/FalconIa/FalconIADom/Tasks/Before10s.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class Before10s : StateMachineBehaviour
+{
+    
+
+    
+
+    // Called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // Called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // Called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
